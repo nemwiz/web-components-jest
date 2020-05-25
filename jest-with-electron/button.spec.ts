abad1f22bc7d8359e459c6ae45f66df9885117a2
neni <HUB4git>
2020-05-25 09:37:43 +0800

    Refactors electron example

--- jest-with-electron/button.spec.ts
+++ jest-with-electron/button.spec.ts
@@ -1,19 +1,36 @@
 import {createAndAppendElement, getShadowRoot} from './test-util';
+import {LitElement} from 'lit-element';
 
 describe('custom-button', () => {
 
-    const CUSTOM_BUTTON_TAG = 'custom-button';
+    const AWESOME_BUTTON_TAG = 'awesome-button';
+    const ELEMENT_ID = 'custom-button';
+    let buttonElement: LitElement;
+
+    beforeEach(() => {
+        buttonElement = createAndAppendElement(AWESOME_BUTTON_TAG);
+    });
+
+    afterEach(() => {
+       document.body.getElementsByTagName(AWESOME_BUTTON_TAG)[0].remove();
+    });
 
     it('displays button text', async () => {
-
-        const buttonElement = createAndAppendElement(CUSTOM_BUTTON_TAG);
         const dummyText = 'Web components & Jest with Electron';
         buttonElement.setAttribute('buttonText', dummyText);
-
         await buttonElement.updateComplete;
 
-        const renderedText = getShadowRoot(CUSTOM_BUTTON_TAG).getElementById('custom-button').innerText;
+        const renderedText = getShadowRoot(AWESOME_BUTTON_TAG).getElementById(ELEMENT_ID).innerText;
 
         expect(renderedText).toEqual(dummyText);
-    })
+    });
+    it('handles clicks', async () => {
+        const mockClickFunction = jest.fn();
+        buttonElement.addEventListener('click', () => {mockClickFunction()});
+
+        getShadowRoot(AWESOME_BUTTON_TAG).getElementById(ELEMENT_ID).click();
+        getShadowRoot(AWESOME_BUTTON_TAG).getElementById(ELEMENT_ID).click();
+
+        expect(mockClickFunction).toHaveBeenCalledTimes(2);
+    });
 });
