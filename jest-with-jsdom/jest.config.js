b8e3c63dc6402d8ee749ee23a076a621b67e0dc4
neni <HUB4git>
2020-05-25 08:22:07 +0800

    Cleans up jsdom folder

--- jest-with-jsdom/jest.config.js
+++ jest-with-jsdom/jest.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom-sixteen',
-  modulePaths: ['./', './dist'],
-  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
-  setupFiles: ['./dist/main.js'],
-  // transformIgnorePatterns: [
-  //   '/node_modules/(?!(lit-element))'
-  // ]
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom-sixteen',
+    setupFiles: ['./dist/main.js']
 };
