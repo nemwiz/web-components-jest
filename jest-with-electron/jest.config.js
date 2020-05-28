dfadb6e57f70fc72fb1154727bcbf050e0de7a92
nemwiz <s3ptease@yahoo.com>
2020-05-28 22:11:16 +0800

    Cleanup

--- jest-with-electron/jest.config.js
+++ jest-with-electron/jest.config.js
@@ -1,16 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
-    // runner: '@jest-runner/electron',
-    // testEnvironment: '@jest-runner/electron/environment',
     runner: 'jest-electron/runner',
     testEnvironment: 'jest-electron/environment',
-    modulePaths: ['./', './dist'],
     setupFiles: ['./dist/main.js'],
-    testMatch: ['**/*.spec.ts'],
-
-    // transformIgnorePatterns: [
-    //     '/node_modules/(?!(lit-element))'
-    //     '/node_modules/(?!(lit-element))',
-    //     // '/node_modules/(?!(lit-html))',
-    // ]
 };
